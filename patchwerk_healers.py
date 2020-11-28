61f7f02f6feb9f6f09bcae25adbd72e94795705c
Anton Chuah <anthony.w.chuah@gmail.com>
2020-11-28 18:24:34 +0800

    add ability for a healer to heal different tanks
    right now it's just a dumb round-robin sequence

--- patchwerk_healers.py
+++ patchwerk_healers.py
@@ -1,6 +1,6 @@
 # NOTE: for priests, add the additional healing from spirtual guidance directly to plus heal
 
-# healer_id : [heal_used, tank_id, +heal, class]
+# healer_id : [heal_used, [tank_sequence], +heal, class]
 # priest: h4, shaman: h2, druid: ?
 # in reality we may be running 10 healers, with 1 druid "floating"
 # we could simulate it as a "boost" to the healing powers of the other healers
@@ -13,16 +13,16 @@
 # INFO | LubbyLubba (9500 / 9500): 16.5% | patchwerk-sim.py:281 <module>()
 
 heals_config = [
-    ['h4', 0, 1160, 'priest'],
-    ['h4', 0, 1160, 'priest'],
-    ['h4', 0, 1160, 'priest'],
-    ['ht4', 0, 1030, 'druid'],
-    ['h2', 1, 1160, 'priest'],
-    ['h2', 1, 1160, 'priest'],
-    ['ht4', 1, 1030, 'druid'],
-    ['h2', 2, 1160, 'priest'],
-    ['h2', 2, 1160, 'priest'],
-    ['h2', 2, 1160, 'priest'],
+    ['h4', [0], 1160, 'priest'],
+    ['h4', [0], 1160, 'priest'],
+    ['h4', [0], 1160, 'priest'],
+    ['h2', [1], 1160, 'priest'],
+    ['h2', [1], 1160, 'priest'],
+    ['ht4', [1], 1030, 'druid'],
+    ['h2', [2], 1160, 'priest'],
+    ['h2', [2], 1160, 'priest'],
+    ['h2', [2], 1160, 'priest'],
+    ['ht4', [1, 2], 1030, 'druid'],
 ]
 
 
@@ -34,15 +34,15 @@ heals_config = [
 # INFO | LubbyLubba (7998.357142857143 / 9500): 17.0% | patchwerk-sim.py:281 <module>()
 """
 heals_config = [
-    ['h4', 0, 1000, 'priest'],
-    ['h2', 0, 1000, 'shaman'],
-    ['h2', 0, 1000, 'shaman'],
-    ['h4', 1, 1250, 'priest'],
-    ['h2', 1, 1250, 'shaman'],
-    ['h2', 1, 1250, 'shaman'],
-    ['h4', 2, 1250, 'priest'],
-    ['h4', 2, 1250, 'priest'],
-    ['h2', 2, 1250, 'shaman'],
+    ['h4', [0], 1000, 'priest'],
+    ['h2', [0], 1000, 'shaman'],
+    ['h2', [0], 1000, 'shaman'],
+    ['h4', [1], 1250, 'priest'],
+    ['h2', [1], 1250, 'shaman'],
+    ['h2', [1], 1250, 'shaman'],
+    ['h4', [2], 1250, 'priest'],
+    ['h4', [2], 1250, 'priest'],
+    ['h2', [2], 1250, 'shaman'],
 ]
 """
 
@@ -52,19 +52,19 @@ heals_config = [
 # INFO | Doodoobear (11000 / 11000): 50.0% | patchwerk-sim.py:281 <module>()
 # INFO | Cowchoppar (9900 / 9900): 32.5% | patchwerk-sim.py:281 <module>()
 # INFO | LubbyLubba (9500 / 9500): 17.5% | patchwerk-sim.py:281 <module>()
-# """
-# heals_config = [
-#     ['h4', 0, 1167, 'priest'],
-#     ['h2', 0, 1167, 'shaman'],
-#     ['h2', 0, 1167, 'shaman'],
-#     ['h4', 1, 1167, 'priest'],
-#     ['h2', 1, 1167, 'shaman'],
-#     ['h2', 1, 1167, 'shaman'],
-#     ['h4', 2, 1167, 'priest'],
-#     ['h4', 2, 1167, 'priest'],
-#     ['h2', 2, 1167, 'shaman'],
-# ]
-# """
+"""
+heals_config = [
+    ['h4', [0], 1167, 'priest'],
+    ['h2', [0], 1167, 'shaman'],
+    ['h2', [0], 1167, 'shaman'],
+    ['h4', [1], 1167, 'priest'],
+    ['h2', [1], 1167, 'shaman'],
+    ['h2', [1], 1167, 'shaman'],
+    ['h4', [2], 1167, 'priest'],
+    ['h4', [2], 1167, 'priest'],
+    ['h2', [2], 1167, 'shaman'],
+]
+"""
 
 # fat bear scenario: 10 healers, with 4 healers on the fat bear
 # INFO | Number of times tanks survived: 990 (99.0%) | patchwerk-sim.py:271 <module>()
@@ -74,15 +74,15 @@ heals_config = [
 # INFO | LubbyLubba (9500 / 9500): 13.5% | patchwerk-sim.py:281 <module>()
 """
 heals_config = [
-    ['h4', 0, 1000, 'priest'],
-    ['h2', 0, 1000, 'shaman'],
-    ['h2', 0, 1000, 'shaman'],
-    ['h2', 0, 1000, 'druid'],
-    ['h4', 1, 1000, 'priest'],
-    ['h2', 1, 1000, 'shaman'],
-    ['h2', 1, 1000, 'shaman'],
-    ['h4', 2, 1000, 'priest'],
-    ['h4', 2, 1000, 'priest'],
-    ['h2', 2, 1000, 'shaman'],
+    ['h4', [0], 1000, 'priest'],
+    ['h2', [0], 1000, 'shaman'],
+    ['h2', [0], 1000, 'shaman'],
+    ['h2', [0], 1000, 'druid'],
+    ['h4', [1], 1000, 'priest'],
+    ['h2', [1], 1000, 'shaman'],
+    ['h2', [1], 1000, 'shaman'],
+    ['h4', [2], 1000, 'priest'],
+    ['h4', [2], 1000, 'priest'],
+    ['h2', [2], 1000, 'shaman'],
 ]
 """
